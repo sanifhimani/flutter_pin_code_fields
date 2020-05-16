13cbd96f0a084494fa6f398689bfb399606412d1
Sanif Himani <sanifhimani92@gmail.com>
2020-05-16 15:09:31 -0400

    Updated Example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,11 +29,13 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text('Pin Code Fields Demo'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: ListView(
           children: <Widget>[
+            SizedBox(
+              height: 30.0,
+            ),
             Text(
-              'Custom Pincode Fields',
+              'Default Pincode Fields',
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 18,
@@ -43,29 +45,29 @@ class _MyHomePageState extends State<MyHomePage> {
             SizedBox(
               height: 40.0,
             ),
-//            PinCodeFields(
-//              length: 4,
-//              controller: newTextEditingController,
-//              focusNode: focusNode,
-//              onComplete: (result) {
-//                // Your logic with code
-//                print(result);
-//              },
-//            ),
-//            SizedBox(
-//              height: 50.0,
-//            ),
-//            Text(
-//              'Custom Pincode Fields',
-//              textAlign: TextAlign.center,
-//              style: TextStyle(
-//                fontSize: 18,
-//                fontWeight: FontWeight.bold,
-//              ),
-//            ),
-//            SizedBox(
-//              height: 10.0,
-//            ),
+            PinCodeFields(
+              length: 4,
+              controller: newTextEditingController,
+              focusNode: focusNode,
+              onComplete: (result) {
+                // Your logic with code
+                print(result);
+              },
+            ),
+            SizedBox(
+              height: 80.0,
+            ),
+            Text(
+              'Obscure Pincode Fields',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
             PinCodeFields(
               length: 6,
               obscureText: true,
@@ -74,6 +76,46 @@ class _MyHomePageState extends State<MyHomePage> {
                 // Your logic with pin code
                 print(text);
               },
+            ),
+            SizedBox(
+              height: 80.0,
+            ),
+            Text(
+              'Custom Pincode Fields',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            PinCodeFields(
+              length: 4,
+              fieldBorderStyle: FieldBorderStyle.Square,
+              responsive: false,
+              fieldHeight: 130.0,
+              fieldWidth: 130.0,
+              borderWidth: 5.0,
+              activeBorderColor: Colors.teal,
+              activeBackgroundColor: Colors.tealAccent,
+              borderRadius: BorderRadius.circular(20.0),
+              keyboardType: TextInputType.number,
+              autoHideKeyboard: false,
+              fieldBackgroundColor: Colors.lightGreenAccent,
+              borderColor: Colors.lightGreen,
+              textStyle: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+              onComplete: (output) {
+                // Your logic with pin code
+                print(output);
+              },
+            ),
+            SizedBox(
+              height: 50.0,
             ),
           ],
         ));
